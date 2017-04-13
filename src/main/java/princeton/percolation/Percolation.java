a47e33ffcb3639208d992ebe26dfd969f452db42
Adrian <adrian>
2017-04-13 13:32:19 +0200

    Fix a bug in the percolation class.

--- src/main/java/princeton/percolation/Percolation.java
+++ src/main/java/princeton/percolation/Percolation.java
@@ -4,13 +4,15 @@ import edu.princeton.cs.algorithms.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private WeightedQuickUnionUF quickUnion;
-    private boolean[] ifOpened; // Array of closed and opened sites. Used for
-    // simulation.
+    private WeightedQuickUnionUF sitesToVirtualTop; // object to visualize opened sites
+    private WeightedQuickUnionUF percolationOccurrence; // object created to check if percolation occurs
+    private boolean[] ifOpened; // Array of closed and opened sites. Used for simulation.
     private int n; // Dimension
     private int openedSites; // Number of opened sites
 
     public Percolation(int n) {
+
+
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
@@ -19,28 +21,10 @@ public class Percolation {
 
         ifOpened = new boolean[n * n]; // Init array of false values
 
-        quickUnion = new WeightedQuickUnionUF(n * n + 2); // Init API of
-        // weighted quick
-        // union algorithm
+        percolationOccurrence = new WeightedQuickUnionUF(n * n + 2); // object which checks if percolation occurs
 
-        // Virtual top site
+        sitesToVirtualTop = new WeightedQuickUnionUF(n * n + 1); // n*n sites and virtual top site
 
-        for (int i = 0; i < n; i++) {
-
-            quickUnion.union(n * n, i); // Union the first n sites
-        }
-
-        //	System.out.println(quickUnion.count());
-
-        // Virtual bottom site
-
-        for (int i = 1; i < n + 1; i++) { // Iterate n times
-
-            quickUnion.union((n * n) - i, n * n + 1); // Union the last n sites
-
-        }
-
-        //	System.out.println(quickUnion.count());
     }
 
     public void open(int row, int col) {
@@ -48,24 +32,39 @@ public class Percolation {
         if (row < 1 || row > n || col < 1 || col > n)
             throw new IndexOutOfBoundsException();
 
-        int position = n * (row - 1) + (col - 1);
+        int position = countIndex(row, col);
 
         if (!ifOpened[position]) {
 
-            if (row > 1 && ifOpened[position - n]) {
-                quickUnion.union(position, position - n);
+            //virtual top site for percolation and visualization
+            if (row == 1) {
+                percolationOccurrence.union(position, n * n);
+                sitesToVirtualTop.union(position, n * n);
             }
 
-            if (col != 1 && ifOpened[position - 1]) {
-                quickUnion.union(position, position - 1);
+            // virtual bottom site for percolation
+            if (row == n) {
+                percolationOccurrence.union(position, n * n + 1);
             }
 
-            if (col != n && ifOpened[position + 1]) { // ???
-                quickUnion.union(position, position + 1);
+            if (row > 1 && ifOpened[position - n]) { // north
+                percolationOccurrence.union(position, position - n);
+                sitesToVirtualTop.union(position, position - n);
             }
 
-            if (row < n && ifOpened[position + n]) {
-                quickUnion.union(position, position + n);
+            if (col != 1 && ifOpened[position - 1]) { // west
+                percolationOccurrence.union(position, position - 1);
+                sitesToVirtualTop.union(position, position - 1);
+            }
+
+            if (col != n && ifOpened[position + 1]) { // east
+                percolationOccurrence.union(position, position + 1);
+                sitesToVirtualTop.union(position, position + 1);
+            }
+
+            if (row < n && ifOpened[position + n]) { // south
+                percolationOccurrence.union(position, position + n);
+                sitesToVirtualTop.union(position, position + n);
             }
 
 
@@ -79,7 +78,7 @@ public class Percolation {
         if (row < 1 || row > n || col < 1 || col > n)
             throw new IndexOutOfBoundsException();
 
-        int position = n * (row - 1) + (col - 1);
+        int position = countIndex(row, col);
         return ifOpened[position];
     }
 
@@ -88,7 +87,7 @@ public class Percolation {
         if (row < 1 || row > n || col < 1 || col > n)
             throw new IndexOutOfBoundsException();
 
-        return isOpen(row, col) && quickUnion.connected(n * (row - 1) + (col - 1), n * n);
+        return isOpen(row, col) && sitesToVirtualTop.connected(countIndex(row, col), n * n);
 
     }
 
@@ -97,9 +96,13 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        if(n == 1 && !this.isOpen(1, 1))
+        if (n == 1 && !this.isOpen(1, 1)) // corner case when array's size is 1
             return false;
-        return quickUnion.connected(n * n, n * n + 1);
+        return percolationOccurrence.connected(n * n, n * n + 1); // check if virtual top site and virtual bottom site are connected
+    }
+
+    private int countIndex(int row, int col) {
+        return n * (row - 1) + (col - 1); // count index in object
     }
 
 }
